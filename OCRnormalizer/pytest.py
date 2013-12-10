27af2d40c60608c27979e77cbea021174b3aa8ff
Ted Underwood <tunder@illinois.edu>
2013-12-09 22:15:50 -0600

    Removed test files.

--- OCRnormalizer/pytest.py
+++
@@ -1,8 +0,0 @@
-def initialize(value):
-    global globalvalue
-    globalvalue = value
-
-
-def multiply(newvalue):
-    global globalvalue
-    return newvalue * globalvalue
--- OCRnormalizer/pytester.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-pytest.initialize(3)
-
-print(pytest.multiply(2))
